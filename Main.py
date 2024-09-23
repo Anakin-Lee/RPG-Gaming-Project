b7ca6aa581cd03ed23f81d125c3db3a8b01566ff
Anakin Lee <anakinlee.2002@gmail.com>
2024-09-23 13:39:44 -0600

    worked on the begging on story nodes

--- Main.py
+++ Main.py
@@ -112,7 +112,7 @@ class Enemy:
 class Gnome(Enemy):
 
     #level one enemies
-    
+
     def __init__(self):
         super().__init__(health = 10, baseAtk = 1, mana = 2, skill = 3)
 
@@ -186,38 +186,66 @@ def draw_characters():
     return
 
 
+def choose_story(input):
+    if input == "Lost Throne":
+        return
+    elif input == "King of Tournaments":
+        return
+
+
+
+
 # Define the story nodes
 story_nodes = {
+
     "start": StoryNode(
+        "Hello Player! Welcome to our Game!!!",
+        {"start game": "choose_bg", "quit game": "are_u_sure"}, 
+        actions={},
+    ),
+    "are_u_sure": StoryNode(
+        "ARE YOU SURE YOU WANT TO EMBRACE COWARDICE YOU LITTLE CHILD BABY", 
+        {"YES": "quit", "NO": "start1"}, 
+        actions={"YES": [quit]},
+    ),
+    "quit": StoryNode(
+        "", 
+        {}, 
+        actions={},
+    ),
+     "choose_bg": StoryNode(
+        "Choose your background of your character(This will dictate the story)", 
+        {"Lost Throne": "choose_ch", "King of Tournaments": "choose_ch"}, 
+        actions={"Lost Throne": [choose_story("Lost Throne")], "King of Tournaments": [choose_story("King of Tournaments")] },
+    ),
+     "choose_ch": StoryNode(
         "Hello adventurer! Choose your class:", 
         {"Mage": "explore_room", "Knight": "explore_room", "Archer": "explore_room", "Unfortunate": "explore_room"},
         actions={"Mage": [player.set_mage], "Knight": [player.set_knight], "Archer": [player.set_archer], "Unfortunate": [player.set_unfortunate]},
-    ),
-    "explore_room": StoryNode(
-        "You find a mysterious key. What now?", 
-        {"take key": "to_door", "leave key": "to_door"},
-        actions={"take key": [found_key, lambda: player.addItem('key'),special_message]}
-        
-    ),
-    "go_back_to_sleep": StoryNode(
-        "You fall back asleep. What now?", 
-        {"wake up": "start"},
-        actions={"wake up": [went_back_to_sleep]}
-    ),
-    "to_door": StoryNode(
-    "You arrive at the door, what do you do?", 
-    {"open the door": "open_door", "go back": "start"},
-    ),
-    "open_door": StoryNode(
-        "You open the door and step into the unknown. End.",
-         {"continue": [lambda: print("Thank you for playing!")]},
-        
-    ),
-
+    )
 }
-battle_node = {
-
+lost_throne_nodes = {
+    "quit": StoryNode(
+        "", 
+        {}, 
+        actions={},
+    ),
+    "quit": StoryNode(
+        "", 
+        {}, 
+        actions={},
+    ),"quit": StoryNode(
+        "", 
+        {}, 
+        actions={},
+    ),"quit": StoryNode(
+        "", 
+        {}, 
+        actions={},
+    ),
 }
+
+
 
 # Initialize Pygame
 pygame.init()
